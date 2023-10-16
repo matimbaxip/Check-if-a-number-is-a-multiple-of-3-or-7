5f74bb5d0b52b1b949bb39918e25d31bada3d3fd
Dell-User <Dell-User@DESKTOP-4LNHG9G>
2023-10-16 15:05:08 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Check_if_a_number_is_a_multiple_of_3_or_7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a positive number:");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            bool result = IsMultipleOfThreeOrSeven(number);
+
+            Console.WriteLine("Result: " + result);
+
+        }
+        static bool IsMultipleOfThreeOrSeven(int number)
+        {
+            return (number % 3 == 0) || (number % 7 == 0);
+        }
+
+    }
+}
